16d3ede7cdcc3fab9239b851f8ffee05c2e3c96f
primelyw <32811464+primelyw@users.noreply.github.com>
2018-09-13 11:25:19 +0800

    basic chat room

--- client.py
+++ client.py
@@ -1,13 +1,35 @@
 import socket
+import threading
 
 host = 'localhost'
-port = 64000
+port = 63000
 
-with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as ct:
-    ct.connect((host,port))
-    d1 = input()
-    d2 = input()
-    ct.sendall(d1.encode())
-    ct.sendall(d2.encode())
-    rt = ct.recv(100).decode()
-    print('%d + %d = %d'%(int(d1),int(d2),int(rt)))
+def listen(ct):
+    while True:
+        data = ct.recv(1000).decode()
+        if data == 'quit':
+            print('Yout friend quits the chat room')
+            break
+        else:
+            print(data)
+
+def write(ct):
+    while True:
+        data = input().encode()
+        ct.sendall(data)
+        if data =='quit':
+            break
+
+def main():
+    with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as ct:
+        ls = threading.Thread(target = listen,args = (ct,))
+        wt = threading.Thread(target= write,args= (ct,))
+        ct.connect((host,port))
+        ls.start()
+        wt.start()
+        ls.join()
+        wt.join()
+        ct.close()
+
+if __name__ == '__main__':
+    main()
--- server.py
+++ server.py
@@ -1,23 +1,41 @@
 import socket
+import threading
 
 host = 'localhost'
-port = 64000
+port = 63000
 
-with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as sr:
-    sr.bind((host,port))
-    sr.listen(5)
+def listen(ct):
     while True:
-        print("Waiting to connect with...")
-        ct,addr = sr.accept()
-        print(addr)
-        with ct:
-            while True:
-                d1 = ct.recv(100).decode()
-                d2 = ct.recv(100).decode()
-                if not d1:
-                    break
-                rep = int(d1)+int(d2)
-                ct.sendall(str(rep).encode())
-                print('Disconnect with',addr)
+        data = ct.recv(1000).decode()
+        if data == 'quit':
+            print('Yout friend quits the chat room')
+            break
+        else:
+            print(data)
+
+def write(ct):
+    while True:
+        data = input()
+        ct.sendall(data.encode())
+        if(data=='quit'):
+            break
+
+def main():
+    with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as sr:
+        sr.bind((host,port))
+        sr.listen()
+        while True:
+            print("Waiting to connect with...")
+            ct,addr = sr.accept()
+            print("Connected with",addr)
+            ls = threading.Thread(target=listen,args=(ct,))
+            wt = threading.Thread(target=write,args = (ct,))
+            ls.start()
+            wt.start()       
+            print("Disconnected with",addr)
+        sr.close()
+
             
             
+if __name__ == '__main__':
+    main()
